317417751a803a59249e633d68f25ffc4a3023d5
thangcn1943 <146865853+thangcn1943@users.noreply.github.com>
2024-09-13 16:52:52 +0700

    Update

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from routers.image_router import image_router
+from routers.id_router import id_router
+from routers.measure import measure_router
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(image_router)
-
+app.include_router(id_router)
+app.include_router(measure_router)
