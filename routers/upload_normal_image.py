01067028a512a950ab7c8c05ca458c3bf0516969
thangcn1943 <146865853+thangcn1943@users.noreply.github.com>
2024-11-12 10:42:55 +0700

    up date

--- routers/upload_normal_image.py
+++ routers/upload_normal_image.py
@@ -15,7 +15,7 @@ def analyze_image(image_data, image_id):
     image, mask = predict_mask(image_data)
     
     # Predict cell types using the CNN model
-    normal, abnormal, normal_2x, abnormal_2x, bounding_boxes,contours_list = predict_cell(image, mask, cnn)
+    normal, abnormal, normal_2x, abnormal_2x, bounding_boxes,contours_list = predict_cell(image,image_id, mask, cnn)
     
     response_content = {
         "image_id": image_id,
